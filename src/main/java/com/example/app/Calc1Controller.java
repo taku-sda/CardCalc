e28ec04cb5505ec549d8d2c0db56ec08e3d9bbff
taku-sda <t.suda.2196@gmail.com>
2020-09-24 17:05:36 +0900

    Calc1Controllerをリファクタリング

--- src/main/java/com/example/app/Calc1Controller.java
+++ src/main/java/com/example/app/Calc1Controller.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/Calc1")
 public class Calc1Controller {
+	/** 山札の初期値. */
+	private static final int DEFAULT_DECK_NUMBER = 40;
+	/** 目的のカード枚数の初期値. */
+	private static final int  DEFAULT_TARGET_NUMBER = 4;
+	/** 引く枚数の初期値. */
+	private static final int  DEFAULT_DRAW_NUMBER = 5;
+	/** 引きたい枚数の初期値. */
+	private static final int  DEFAULT_WANT_NUMBER = 1;
 	
 	@ModelAttribute
 	public Calc1Form setUpform() {
 		Calc1Form form = new Calc1Form();
-		form.setDeck(40);
-		form.setTarget(4);
-		form.setDraw(5);
-		form.setWant(1);
+		form.setDeck(DEFAULT_DECK_NUMBER);
+		form.setTarget(DEFAULT_TARGET_NUMBER);
+		form.setDraw(DEFAULT_DRAW_NUMBER);
+		form.setWant(DEFAULT_WANT_NUMBER);
 		return form;
 	}
 	
