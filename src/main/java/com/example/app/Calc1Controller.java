32ece07c211d68364a9b03ff8cd3362e4c34fc5d
taku-sda <t.suda.2196@gmail.com>
2020-09-24 16:36:52 +0900

    Calc1Formへの初期値の格納及びclac1.htmlのフォームとの連携を実装
    また、index.htmlのリンクの誤りを修正

--- src/main/java/com/example/app/Calc1Controller.java
+++ src/main/java/com/example/app/Calc1Controller.java
@@ -3,11 +3,23 @@ package com.example.app;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/Calc1")
 public class Calc1Controller {
+	
+	@ModelAttribute
+	public Calc1Form setUpform() {
+		Calc1Form form = new Calc1Form();
+		form.setDeck(40);
+		form.setTarget(4);
+		form.setDraw(5);
+		form.setWant(1);
+		return form;
+	}
+	
 	@GetMapping
 	public String form(Model model) {
 		model.addAttribute("title", "CardCalc | Calc1");
