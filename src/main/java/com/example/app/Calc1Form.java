f06a38eb19e487d1063c16cd0e1fa5cb89d4590d
taku-sda <t.suda.2196@gmail.com>
2020-09-25 14:08:18 +0900

    フォームクラスに不足していたバリデーションを追加

--- src/main/java/com/example/app/Calc1Form.java
+++ src/main/java/com/example/app/Calc1Form.java
@@ -3,27 +3,34 @@ package com.example.app;
 import java.io.Serializable;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import com.example.validation.DeckOrLess;
 
-@DeckOrLess(property = "taget", comparingProperty = "deck", message = "目的のカードの枚数は山札以下にしてください")
-@DeckOrLess(property = "draw", comparingProperty = "deck", message = "引く枚数は山札以下にしてください")
-@DeckOrLess(property = "want", comparingProperty = "target", message = "引きたい枚数は目的のカードの枚数以下にしてください")
-public class Calc1Form implements Serializable{
+@DeckOrLess(property = "taget", comparingProperty = "deck",
+	message = "目的のカードの枚数は山札以下にしてください")
+@DeckOrLess(property = "draw", comparingProperty = "deck",
+	message = "引く枚数は山札以下にしてください")
+@DeckOrLess(property = "want", comparingProperty = "target",
+	message = "引きたい枚数は目的のカードの枚数以下にしてください")
+public class Calc1Form implements Serializable {
 	private static final long serialVersionUID = 1L;
+	/** 山札の最大枚数. */
+	private static final int MAX_DECK_NUMBER = 60;
 	
-	@NotNull
-	@Max(60)
+	@NotNull(message = "枚数を入力してください")
+	@Max(value = MAX_DECK_NUMBER, message = "山札は60枚以下です")
+	@Min(value = 1, message = "1枚以上にしてください")
 	private int deck;
-	
-	@NotNull
+	@NotNull(message = "枚数を入力してください")
+	@Min(value = 1, message = "1枚以上にしてください")
 	private int target;
-	
-	@NotNull
+	@NotNull(message = "枚数を入力してください")
+	@Min(value = 1, message = "1枚以上にしてください")
 	private int draw;
-	
-	@NotNull
+	@NotNull(message = "枚数を入力してください")
+	@Min(value = 1, message = "1枚以上にしてください")
 	private int want;
 	
 	public int getDeck() {
