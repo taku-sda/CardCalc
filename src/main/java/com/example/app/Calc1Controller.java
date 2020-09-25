f015e77117aca63089714497c875f4486d5e6bc5
taku-sda <t.suda.2196@gmail.com>
2020-09-25 15:25:28 +0900

    Clac1ControllerにdisplayResultメソッドを追加
    計算結果を格納して表示画面に遷移する

--- src/main/java/com/example/app/Calc1Controller.java
+++ src/main/java/com/example/app/Calc1Controller.java
@@ -1,10 +1,16 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.service.Calc1Service;
 
 @Controller
 @RequestMapping("/Calc1")
@@ -17,6 +23,13 @@ public class Calc1Controller {
 	private static final int  DEFAULT_DRAW_NUMBER = 5;
 	/** 引きたい枚数の初期値. */
 	private static final int  DEFAULT_WANT_NUMBER = 1;
+	
+	private final Calc1Service calc1Service;
+	
+	@Autowired
+	public Calc1Controller(Calc1Service calc1Service) {
+		this.calc1Service = calc1Service;
+	}
 	
 	@ModelAttribute
 	public Calc1Form setUpform() {
@@ -32,5 +45,19 @@ public class Calc1Controller {
 	public String form(Model model) {
 		model.addAttribute("title", "CardCalc | Calc1");
 		return "calc1";
+	}
+	
+	@PostMapping
+	public String displayResult(@Validated Calc1Form form, BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			return form(model);
+		}
+		double calcResult = calc1Service.calculate(form.getDeck(), form.getTarget(),
+				form.getDraw(), form.getWant());
+		//求めた確率は小数点以下2桁になるように四捨五入して格納
+		model.addAttribute("calcResult", String.format("%.2f", calcResult));
+		model.addAttribute("title", "CardCalc | Calc1");
+		return"calc1";
 	}
 }
